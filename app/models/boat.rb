a0257932942a42c0626ba39683f29713210872fb
benjamin maureau <benjamin.maureau@hotmail.fr>
2021-06-14 16:09:14 +0200

    boat model still invalid

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,7 +1,6 @@
 class Boat < ApplicationRecord
-  belongs_to :user
 
-  validates :type, :brand, :location, :length, :price_per_day, :capacity, :building_year, :engine_power, presence: true
-  validates :type, acceptance: { accept: ['motorboat', 'sailboat']}
-  validates :capacity, numericality: {greater_than: 0}
+  validates :category, :brand, :location, :length, :price_per_day, :capacity, :building_year, :engine_power, presence: true
+  validates :category, acceptance: { accept: ["motorboat", "sailboat"]}
+  validates :capacity, numericality: { greater_than: 0 }
 end
