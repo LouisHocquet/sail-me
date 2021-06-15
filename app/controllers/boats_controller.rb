04efbcae55b16498dad81cb575099e9ed217b2b6
benjamin maureau <benjamin.maureau@hotmail.fr>
2021-06-15 10:03:10 +0200

    notice messages

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,4 +1,5 @@
 class BoatsController < ApplicationController
+  before_action :set_id, only: [:show, :edit, :update, :destroy]
   def index
     @boats = Boat.all
   end
@@ -8,7 +9,6 @@ class BoatsController < ApplicationController
   end
 
   def show
-    @boat = Boat.find(params[:id])
   end
 
   def create
@@ -16,34 +16,36 @@ class BoatsController < ApplicationController
     @boat.user = current_user
 
     if @boat.save
-      redirect_to boat_path(@boat)
+      redirect_to boat_path(@boat), notice: "Your boat has been added successfully."
     else
       render 'boats/show'
     end
   end
 
   def edit
-    @boat = Boat.find(params[:id])
   end
 
   def update
-    @boat = Boat.find(params[:id])
     @boat.update(boat_params)
     if @boat.save
-      redirect_to boat_path(@boat)
+      redirect_to boat_path(@boat), notice: "Your boat has been updated successfully."
     else
       render 'new'
     end
   end
 
   def destroy
-    @boat = Boat.find(params[:id])
     @boat.destroy
 
-    redirect_to boats_path
+    redirect_to boats_path, notice: "Your boat has been deleted successfully."
+
   end
 
   private
+
+  def set_id
+    @boat = Boat.find(params[:id])
+  end
 
   def boat_params
     params.require(:boat).permit(:category, :brand, :location, :length, :price_per_day, :capacity, :building_year, :engine_power)
