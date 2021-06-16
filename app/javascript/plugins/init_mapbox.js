d62fbd9d167f844da6a236598dfbdb143d1b70d5
Louis Hocquet <louis.hocquet@edhec.com>
2021-06-16 12:13:57 +0200

    Basic map ready

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,7 +8,8 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v10',
+      // zoom: 5
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
@@ -25,7 +26,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5 });
   };
 
 };
