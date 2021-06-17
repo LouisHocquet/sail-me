192e2bc8e5c7ecaf3a2cfbeb30f9b3dfda501b44
Robin Dahan <robin.dahan@edhec.com>
2021-06-17 19:13:28 +0200

    Search v1 voir boat controller

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :boat
   validates :start_date, :end_date, :status, presence: true
   validates :status, acceptance: { accept: ["Pending", "Accepted", "Rejected"] }
-  include PgSearch::Model
-  multisearchable against: [:user, :boat]
 end
