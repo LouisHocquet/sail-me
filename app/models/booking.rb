6a2fa15ff4ea63ebf3e9c9d15fe7b029228d33a9
Robin Dahan <robin.dahan@edhec.com>
2021-06-17 18:27:24 +0200

    search first try

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :boat
   validates :start_date, :end_date, :status, presence: true
   validates :status, acceptance: { accept: ["Pending", "Accepted", "Rejected"] }
+  include PgSearch::Model
+  multisearchable against: [:user, :boat]
 end
