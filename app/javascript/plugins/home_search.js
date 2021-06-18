a46fe73a2d4d34b6c7d03f5fa082edf9d60aa73b
Robin Dahan <robin.dahan@edhec.com>
2021-06-18 13:02:55 +0200

    button js class cassé

---
+++ app/javascript/plugins/home_search.js
@@ -0,0 +1,20 @@
+const activeCategory = (category) => {
+  const category = document.querySelector(`#category-${category}-boat.active`);
+  category.classList.toggle('active');
+};
+
+const homeSearch = () => {
+  const btnSearch = document.getElementById('home-search-button');
+  const boatCategory = document.getElementById('home-search-category');
+
+// fonction qui change la classe du bouton en fonction de s'il est cliqué ou non
+// toggle sur chaque category
+// récupérer le contenu du bouton qui a la classe active
+// query selector all
+  btnSearch.addEventListener('click', (event) => {
+    console.log(event.currentTarget);
+  });
+};
+
+export { activeCategory };
+export { homeSearch };
